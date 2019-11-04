562ce589f2f762d1e9f5a4ed391303c1bd1e213f
Sahar Elsayed <Sahar.Elsayed@ibtikar.com>
2019-11-04 13:23:22 +0200

    working on trailer modal and button

--- src/components/cardSingle/CardSingle.js
+++ src/components/cardSingle/CardSingle.js
@@ -13,151 +13,166 @@ const CastList = React.lazy(() => import('./../castList/CastList'));
 const Button = React.lazy(() => import('./../button/Button'));
 const VideoModal = React.lazy(() => import('./../../containers/videoModal/VideoModal'));
 
-// Render info of movie
-const renderInfo = (languages, time, data) => {
-  const info = [];
-  if (languages && languages.length !== 0) {
-    info.push(languages[0].name);
+class CardSingle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false
+    }
   }
-  info.push(time, data);
-  return info
-    .filter(el => el !== null)
-    .map(el => (typeof el === 'number' ? `${el} min.` : el))
-    .map((el, i, array) => (i !== array.length - 1 ? `${el} / ` : el));
-}
 
-// Function to get the year only from the date
-const splitYear = (date) => {
-  if (!date) {
-    return;
+  renderInfo = (languages, time, data) => {
+    const info = [];
+    if (languages && languages.length !== 0) {
+      info.push(languages[0].name);
+    }
+    info.push(time, data);
+
+    return info
+      .filter(el => el !== null)
+      .map(el => (typeof el === 'number' ? `${el} min.` : el))
+      .map((el, i, array) => (i !== array.length - 1 ? `${el} / ` : el));
   }
-  const [year] = date.split('-');
-  return year;
-}
 
-// Render Genres with links
-const renderGenres = (genres) => {
-  if (genres) {
-    return genres.map((genre, i) => (
-      <div className="text-light font-weight-bold pr-3" key={i}>
-        <FontAwesomeIcon
-          icon={faDotCircle}
-          size='1x'
-          className="mr-1"
-        />
-        <span>{genre.name}</span>
-      </div>
-    ));
+  splitYear = (date) => {
+    if (!date) {
+      return;
+    }
+    const [year] = date.split('-');
+    return year;
   }
-}
 
-// Render Trailer button. On click triggers state to open modal of trailer
-function renderTrailer(videos) {
-  if (videos.length === 0) {
-    return;
-  }
-  const { key } = videos.find(
-    video => video.type === 'Trailer' && video.site === 'YouTube'
-  );
-  return (
-    <React.Fragment>
-      <VideoModal
-        channel="youtube"
-        videoId={key}
-      />
-    </React.Fragment>
-  );
-}
-
-const CardSingle = ({ movie, cast }) => {
-  console.log(movie);
-
-  return (
-    (movie && cast) &&
-    <div className="card-single row justify-content-between align-items-start">
-
-      <div className="card-single__img">
-        <Suspense fallback={<Loader />}>
-          <CardImage src={movie.poster_path ? `${ImgBaseURL}${movie.poster_path}` : `/assets/svgs/nothing.svg`} />
-        </Suspense>
-      </div>
-
-      <div className="card-single__content">
-        <div className="card-single__title mb-5">
-          <h1>{movie.original_title}</h1>
-          <h2>{movie.tagline}</h2>
-        </div>
-
-        <div className="d-flex justify-content-between card-single__rate mb-5">
-          <Rating size='lg' initialRating={movie.vote_average} showAverage />
-          <h3>
-            {renderInfo(
-              movie.spoken_languages,
-              movie.runtime,
-              splitYear(movie.release_date)
-            )}
-          </h3>
-        </div>
-
-        <div className="mb-5">
-          <h3>THE GENRES</h3>
-          <div className="card-single__genres d-flex">
-            {renderGenres(movie.genres)}
-          </div>
-        </div>
-
-        <div className="mb-5">
-          <h3 className="mb-3">THE SYNOPSIS</h3>
-          <p>{movie.overview ? movie.overview : 'There is no synopsis available...'}</p>
-        </div>
-
-        <div className="mb-5">
-          <h3 className="mb-3">THE CAST</h3>
-          <CastList cast={cast} />
-        </div>
-
-        <div className="buttons-wrapper d-flex justify-content-between">
-          <div className='buttons-wrapper__leftBtns d-flex justify-content-between'>
-            {
-              movie.homepage &&
-              <Button
-                text='Website'
-                icon={faLink}
-                className="btn-outline-primary"
-                order='1'
-                size='lg'
-                dir='l'
-                action={() => window.open(movie.homepage, '_blank')}
-              />
-            }
-            {
-              movie.imdb_id &&
-              <Button
-                text='IMDB'
-                icon={faImdb}
-                className="btn-outline-primary"
-                order='1'
-                size='lg'
-                dir='l'
-                action={() => window.open(`https://www.imdb.com/title/${movie.imdb_id}`, '_blank')}
-              />
-            }
-            {renderTrailer(movie.videos.results)}
-          </div>
-          <Button
-            text='Back'
-            icon={faArrowLeft}
-            order='0'
-            size='lg'
-            dir='r'
-            action={history.goBack}
+  renderGenres = (genres) => {
+    if (genres) {
+      return genres.map((genre, i) => (
+        <div className="text-light font-weight-bold pr-3" key={i}>
+          <FontAwesomeIcon
+            icon={faDotCircle}
+            size='1x'
+            className="mr-1"
           />
+          <span>{genre.name}</span>
+        </div>
+      ));
+    }
+  }
+
+  renderTrailer = (videos, isOpen, callBack) => {
+    if (videos.length === 0) {
+      return;
+    }
+    const { key } = videos.find(
+      video => video.type === 'Trailer' && video.site === 'YouTube'
+    );
+    return (
+      <React.Fragment>
+        <VideoModal
+          channel="youtube"
+          videoId={key}
+          action={callBack}
+          isOpen={isOpen}
+        />
+      </React.Fragment>
+    );
+  }
+
+  ChangeModalState = () => {
+    this.setState({ isOpen: !this.state.isOpen });
+  }
+
+  render() {
+    const { movie, cast } = this.props;
+    const { isOpen } = this.state;
+    return (
+      (movie && cast) &&
+      <div className="card-single row justify-content-between align-items-start">
+
+        <div className="card-single__img">
+          <Suspense fallback={<Loader />}>
+            <CardImage src={movie.poster_path ? `${ImgBaseURL}${movie.poster_path}` : `/assets/svgs/nothing.svg`} />
+          </Suspense>
+        </div>
+
+        <div className="card-single__content">
+          <div className="card-single__title mb-5">
+            <h1>{movie.original_title}</h1>
+            <h2>{movie.tagline}</h2>
+          </div>
+
+          <div className="d-flex justify-content-between card-single__rate mb-5">
+            <Rating size='lg' initialRating={movie.vote_average} showAverage />
+            <h3>
+              {this.renderInfo(
+                movie.spoken_languages,
+                movie.runtime,
+                this.splitYear(movie.release_date)
+              )}
+            </h3>
+          </div>
+
+          <div className="mb-5">
+            <h3>THE GENRES</h3>
+            <div className="card-single__genres d-flex">
+              {this.renderGenres(movie.genres)}
+            </div>
+          </div>
+
+          <div className="mb-5">
+            <h3 className="mb-3">THE SYNOPSIS</h3>
+            <p>{movie.overview ? movie.overview : 'There is no synopsis available...'}</p>
+          </div>
+
+          <div className="mb-5">
+            <h3 className="mb-3">THE CAST</h3>
+            <CastList cast={cast} />
+          </div>
+
+          <div className="buttons-wrapper d-flex justify-content-between">
+            <div className='buttons-wrapper__leftBtns d-flex justify-content-between'>
+
+              {movie.homepage &&
+                <Button
+                  text='Website'
+                  icon={faLink}
+                  className="btn-outline-primary"
+                  order='1'
+                  size='lg'
+                  dir='l'
+                  action={() => window.open(movie.homepage, '_blank')}
+                />
+              }
+
+              {movie.imdb_id &&
+                <Button
+                  text='IMDB'
+                  icon={faImdb}
+                  className="btn-outline-primary"
+                  order='1'
+                  size='lg'
+                  dir='l'
+                  action={() => window.open(`https://www.imdb.com/title/${movie.imdb_id}`, '_blank')}
+                />
+              }
+
+              {this.renderTrailer(movie.videos.results, isOpen, this.ChangeModalState)}
+
+            </div>
+
+            <Button
+              text='Back'
+              icon={faArrowLeft}
+              order='0'
+              size='lg'
+              dir='r'
+              action={history.goBack}
+            />
+          </div>
+
         </div>
 
       </div>
-
-    </div>
-  )
+    )
+  }
 }
 
 export default CardSingle;
--- src/containers/videoModal/VideoModal.js
+++ src/containers/videoModal/VideoModal.js
@@ -3,42 +3,27 @@ import ModalVideo from 'react-modal-video'
 import Button from '../../components/button/Button';
 import { faPlay } from '@fortawesome/free-solid-svg-icons';
 
-class VideoModal extends React.Component {
+const VideoModal = ({ channel, isOpen, videoId, action }) => {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      isOpen: false
-    }
-  }
-
-  openModal = () => {
-    this.setState({ isOpen: true })
-  }
-
-  render() {
-    console.log(this.props);
-
-    return (
-      <div>
-        <ModalVideo
-          channel='youtube'
-          isOpen={this.state.isOpen}
-          videoId={this.props.videoId}
-          onClose={() => this.setState({ isOpen: false })}
-        />
-        <Button
-          text='Trailer'
-          icon={faPlay}
-          className="btn-outline-primary"
-          order='1'
-          size='lg'
-          dir='l'
-          action={this.openModal}
-        />
-      </div>
-    )
-  }
+  return (
+    <React.Fragment>
+      <ModalVideo
+        channel={channel}
+        isOpen={isOpen}
+        videoId={videoId}
+        onClose={action}
+      />
+      <Button
+        text='Trailer'
+        icon={faPlay}
+        className="btn-outline-primary"
+        order='1'
+        size='lg'
+        dir='l'
+        action={action}
+      />
+    </React.Fragment>
+  )
 }
 
 export default VideoModal;
